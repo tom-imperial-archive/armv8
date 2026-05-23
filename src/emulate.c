a3b1277635d3c8709d71308b59cd856cd6066769
Hugo Carter <hjc25@ic.ac.uk>
2026-05-23 22:34:45 +0100

    [Hugo Carter] Add newline to all modified EOF

--- src/emulate.c
+++ src/emulate.c
@@ -5,7 +5,8 @@
 int main(int argc, char **argv)
 {
     State *state = init_state();
-
+    offset_pc(state, 8);
+    print_all_registers(state);
     destroy_state(state);
     return EXIT_SUCCESS;
 }
