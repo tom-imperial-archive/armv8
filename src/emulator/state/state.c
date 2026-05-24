c45035825db2b9825bb43a32329a1b439e05d5f0
Hugo Carter <hjc25@ic.ac.uk>
2026-05-24 16:47:12 +0100

    [Hugo Carter] feat: add PSTATE flags to state

--- src/emulator/state/state.c
+++ src/emulator/state/state.c
@@ -29,7 +29,8 @@ void check_register(Register reg)
     }
 }
 
-void check_writeable_register(Register reg) {
+void check_writeable_register(Register reg)
+{
     check_register(reg);
     if (reg == ZR || reg == PC)
     {
@@ -37,7 +38,8 @@ void check_writeable_register(Register reg) {
     }
 }
 
-uint64* get_register(State *state, Register reg) {
+uint64 *get_register(State *state, Register reg)
+{
     return (uint64 *)state + reg;
 }
 
@@ -54,22 +56,25 @@ void write_reg_32(State *state, Register reg, uint32 val)
     check_writeable_register(reg);
     // We have a writable register
     uint64 *rp = get_register(state, reg);
-    *rp = (uint64) val;
+    *rp = (uint64)val;
 }
 
-uint64 read_reg_64(State *state, Register reg) {
+uint64 read_reg_64(State *state, Register reg)
+{
     check_register(reg);
     return *get_register(state, reg);
 }
 
-uint32 read_reg_32(State *state, Register reg) {
+uint32 read_reg_32(State *state, Register reg)
+{
     check_register(reg);
 
-    if (reg == PC) {
+    if (reg == PC)
+    {
         // todo error handling
     }
 
-    return (uint32) *get_register(state, reg);
+    return (uint32)*get_register(state, reg);
 }
 
 void destroy_state(State *state)
@@ -87,11 +92,43 @@ void inc_pc(State *state)
     state->PC = state->PC + 4;
 }
 
+bool *get_pstate_flag(State *state, PSTATE_flag pstate)
+{
+    return (bool *)state + pstate;
+}
+
+void write_pstate_flag(State *state, PSTATE_flag pstate, bool val)
+{
+    if (pstate < 0 || pstate > V)
+    {
+        // Invalid register. Handle error
+        return;
+    }
+
+    bool *flag = get_pstate_flag(state, pstate);
+    *flag = val;
+}
+
+bool read_pstate_flag(State *state, PSTATE_flag pstate)
+{
+    if (pstate < 0 || pstate > V)
+    {
+        // Invalid register. Handle error
+    }
+
+    return *get_pstate_flag(state, pstate);
+}
+
 void print_all_registers(State *state)
 {
     for (int r = 0; r <= SP; r++)
     {
         uint64 val = read_reg_64(state, r);
         printf("R%d: %ld\n", r, val);
+    }
+    for (int p = 0; p <= V; p++)
+    {
+        bool val = read_pstate_flag(state, p);
+        printf("P%d: %d\n", p, val);
     }
 }
