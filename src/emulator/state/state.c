5e9e6de59dca9720ff4cf8ed34c6f67669d178ad
Hugo Carter <hjc25@ic.ac.uk>
2026-05-25 12:55:15 +0100

    [Hugo Carter] feat: error messages for state

--- src/emulator/state/state.c
+++ src/emulator/state/state.c
@@ -5,6 +5,30 @@
 #define CAT strcat(out, new);
 #define ONES_32BIT (uint32) 0xFFFFFFFFUL
 
+typedef enum {REG_NOT_EXISTS, WRITE_NOT_ALLOWED, READ_32_FROM_PC, FLAG_NOT_EXISTS} StateError;
+
+//todo refactor errors accross the project
+void error(StateError error) {
+    switch(error) {
+        case REG_NOT_EXISTS: printf("Register does not exist"); break;
+        case WRITE_NOT_ALLOWED: printf("Illegal write to register"); break;
+        case READ_32_FROM_PC: printf("Illegal 32-bit read from PC"); break;
+        case FLAG_NOT_EXISTS: printf("PSTATE flag does not exist"); break;
+        default: printf("Unknown error accessing registers");
+    }
+    exit(1);
+}
+
+
+uint64 *get_register(State *state, Register reg)
+{
+    return (uint64 *)state + reg;
+}
+
+void clear_reg(State *state, Register reg) {
+    *get_register(state, reg) = 0;
+}
+
 State *init_state()
 {
     State *state = malloc(sizeof(State));
@@ -13,7 +37,7 @@ State *init_state()
     // Clear all registers except PSTATE
     for (int r = 0; r <= SP; r++)
     {
-        write_reg_64(state, r, 0);
+        clear_reg(state, r);
     }
 
     // Clear PSTATE
@@ -27,9 +51,9 @@ State *init_state()
 
 void check_register(Register reg)
 {
-    if (!(reg < R0) || !(reg > SP))
+    if ((reg < R0) || (reg > SP))
     {
-        // todo error handling
+        error(REG_NOT_EXISTS);
     }
 }
 
@@ -38,19 +62,14 @@ void check_writeable_register(Register reg)
     check_register(reg);
     if (reg == ZR || reg == PC)
     {
-        // todo error handling
+        printf("%d", reg);
+        error(WRITE_NOT_ALLOWED);
     }
-}
-
-uint64 *get_register(State *state, Register reg)
-{
-    return (uint64 *)state + reg;
 }
 
 void write_reg_64(State *state, Register reg, uint64 val)
 {
     check_writeable_register(reg);
-    // We have a writable register
     uint64 *rp = get_register(state, reg);
     *rp = val;
 }
@@ -68,11 +87,9 @@ uint64 read_reg_64(State *state, Register reg)
 
 uint32 read_reg_32(State *state, Register reg)
 {
-    check_register(reg);
-
     if (reg == PC)
     {
-        // todo error handling
+        error(READ_32_FROM_PC);
     }
 
     return read_reg_64(state, reg) & ONES_32BIT;
@@ -99,13 +116,16 @@ bool *get_pstate_flag(State *state, PSTATE_flag pstate)
     return (bool *) &(state->N) + pstate;
 }
 
-void write_pstate_flag(State *state, PSTATE_flag pstate, bool val)
-{
+void check_valid_flag(State *state, PSTATE_flag pstate) {
     if (pstate < 0 || pstate > V)
     {
-        // Invalid register. Handle error
-        return;
+        error(FLAG_NOT_EXISTS);
     }
+}
+
+void write_pstate_flag(State *state, PSTATE_flag pstate, bool val)
+{
+
 
     bool *flag = get_pstate_flag(state, pstate);
     *flag = val;
@@ -113,10 +133,7 @@ void write_pstate_flag(State *state, PSTATE_flag pstate, bool val)
 
 bool read_pstate_flag(State *state, PSTATE_flag flag)
 {
-    if (flag < 0 || flag > V)
-    {
-        // Invalid register. Handle error
-    }
+    check_valid_flag(state, flag);
 
     return *get_pstate_flag(state, flag);
 }
