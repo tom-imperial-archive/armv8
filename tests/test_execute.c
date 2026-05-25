4bf070bc2f3670463953925b1559b7476d047040
Hugo Carter <hjc25@ic.ac.uk>
2026-05-25 16:58:12 +0100

    Execute tests

---
+++ tests/test_execute.c
@@ -0,0 +1,13 @@
+#include "test_execute.h"
+#include <stdio.h>
+#include "common/util.h"
+#include "emulator/decode/execute.h"
+
+void test_execute(void)
+{
+    printf("Execute Tests\n");
+    printf("-------------\n");
+
+    printf("-------------\n");
+    printf("All tests passed\n");
+}
---
+++ tests/test_execute.h
@@ -0,0 +1,1 @@
+void test_execute(void);
