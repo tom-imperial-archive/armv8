2564f65c739d70a937ce4c886e1ab0598fdf36d1
Hugo Carter <hjc25@ic.ac.uk>
2026-05-25 16:12:17 +0100

    [Hugo Carter] feat: add skeleton execution function

---
+++ src/emulator/decode/execute.c
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include "common/util.h"
+#include "emulator/state/state.h"
+#include "emulator/decode.h"
+
+/*
+    Executes the given instruction.
+    If we encounter a halt instruction, we return true, otherwise return false;
+*/
+bool execute_instruction(State *state, OpType op, Instruction i)
+{
+    switch (op)
+    {
+    case OP_TYPE_HALT:
+        return true;
+        // Data processing instruction (immediate)
+        // Data processing instruction (register)
+    }
+    return false;
+}
