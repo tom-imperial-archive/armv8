c69dd5c5c9884c43b55829e969838e60612a63fe
Hugo Carter <hjc25@ic.ac.uk>
2026-05-28 16:37:18 +0100

    [Hugo Carter] feat(assembler): add first parse, saves labels to symbol table

--- tests/assembler/pass1/test_scanner.c
+++ tests/assembler/pass1/test_scanner.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <assert.h>
+#include "assembler/pass1/scanner.h"
+
+void test_scanfile() {
+    SymbolTable *table = create_symbol_table();
+    scan_file("tests/assembler/pass1/test_scanner.txt", table);
+    assert(symbol_table_lookup(table, ".label1") == 0);
+    assert(symbol_table_lookup(table, "label2") == 12);
+    assert(symbol_table_lookup(table, "_label$") == 20);
+
+    symbol_table_lookup(table, "_label$");
+    free_symbol_table(table);
+    printf("Scanner Test: OK\n");
+}
 
 int main (void)
 {
-    printf("No scanner tests yet!");
+    printf("Scanner Tests\n");
+    printf("-------------\n");
+    test_scanfile();
+    printf("-------------\n");
+    printf("All tests passed\n");
     return 0;
 }
