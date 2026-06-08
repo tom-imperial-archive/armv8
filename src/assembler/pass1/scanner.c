104697cd354a43b32f1369bb7efb6e53144ee1e7
Hugo Carter <hjc25@ic.ac.uk>
2026-06-08 18:00:55 +0100

    [Hugo Carter] refac(assemble): remove non-portable regex

--- src/assembler/pass1/scanner.c
+++ src/assembler/pass1/scanner.c
@@ -7,6 +7,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#define is_label_char(c) isalnum(c) || c == '_' || c == '.' || c == '\\'
+
 #define INSTRUCTION_SIZE 4
 #define MAX_FILE_LINE_LENGTH 120
 #define REGEX_FLAGS (REG_EXTENDED | REG_ICASE)
@@ -14,7 +17,7 @@
 THIS IS PASS 1 OF THE TWO PASS APPROACH
 
 We read through each line, tracking the current memory address (making sure to
-ignore comments). When we enccounter a label, we add it, and the current
+ignore comments). When we encounter a label, we add it, and the current
 address, to the symbol table.
 
 This accepts any preceeding whitespace
@@ -23,43 +26,36 @@ This accepts any preceeding whitespace
 // Regex: [a-zA-Z_\.]([a-zA-Z0-9$_\.])*
 static void read_line(char *buf, uint64 *address, SymbolTable *table) {
     // Remove whitespace
-    while (*buf == '\t' || *buf == ' ' || *buf == '\n')
-        buf++;
+    while (isspace(*buf)) buf++;
 
-    regex_t labelRegex;
-    const char labelExp[] = "^[a-zA-Z_.][a-zA-Z0-9_$.]*:";
-    int labelRes = regcomp(&labelRegex, labelExp, REGEX_FLAGS);
+    printf("Address: 0x%lx\n", *address);
+    printf("Line: %s\n", buf);
 
-    regex_t directiveRegex;
-    const char directiveExp[] = "^[.]([a-zA-Z0-9]+)";
-    int directiveRes = regcomp(&directiveRegex, directiveExp, REG_EXTENDED);
+/*
+If label: don't increment
+If directive: increment
+If instruction: increment*/
 
-    // Assert compilation was successful
-    assert(labelRes == 0);
-    assert(directiveRes == 0);
+    if (*buf == '\0') {
+        // Line is just whitespace
+        return;
+    }
 
-    // printf("Str: %s", buf);
-    regmatch_t match[1];
+    // Line is not whitespace
+    int i = 0;
+    while(is_label_char(buf[i])) {
+        i++;
+    }
 
-    // Check for a label
-    if (regexec(&labelRegex, buf, 1, match, 0) == 0) {
-        // This is a label
-        // Find the length of the matching substring. We remove the trailing :
-        // so subtract 1 from the length.
-        int length = match[0].rm_eo - match[0].rm_so - 1;
-        char label[length + 1];
-        strncpy(label, buf + match[0].rm_so, length);
-        label[length] = '\0';
-
-        symbol_table_add(table, label, *address);
-    } else if (buf[0] != '\0') {
-        // Line is non-empty. We either have a directive or an instruction.
-        // Either way, this corresponds to 4 bytes once assembled
+    if (buf[i] == ':') {
+        // We have a label
+        // Do not increment
+        buf[i] = '\0';
+        symbol_table_add(table, buf, *address);
+    } else {
         *address += 4;
     }
 
-    regfree(&labelRegex);
-    regfree(&directiveRegex);
 }
 
 // This should read through the file, and populate a SymbolTable mapping labels
