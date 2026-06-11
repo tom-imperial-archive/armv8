ccba708806414b339c95e19be68132f28bf8b23c
Hari <hn1125@ic.ac.uk>
2026-06-11 17:00:54 +0100

    [Hari] client connect_to_server function

--- src/extension/client/network.c
+++ src/extension/client/network.c
@@ -1,3 +1,50 @@
 /*
 Checks for updates every frame, without interfering with main loop
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "../shared/log.h"
+
+void error(const char *msg) {
+    perror(msg);
+    printf("Something went wrong\n");
+    exit(0);
+}
+
+int connect_to_server(char *hostname, int port) {
+   
+    struct sockaddr_in server_addr;
+    struct hostent *server;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        error("Error opening socket for server connection");
+    }
+
+    server = gethostbyname(hostname);
+
+    if (server == NULL) {
+        fprintf(stderr, "ERROR: no such host\n");
+        exit(0);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    memmove(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    server_addr.sin_port = htons(port);
+
+    // attempt to connect
+    if (connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        error("Error connecting to server");
+    }
+
+    printf(CONNECTION_SUCCESS_TO_SERVER);
+    return(sockfd);
+}
--- src/extension/shared/log.h
+++ src/extension/shared/log.h
@@ -1,3 +1,6 @@
 /*
 Shared debugging utility, with logging macros
 */
+#define CONNECTION_SUCCESS_TO_SERVER "[DEBUG] Connected to server\n"
+#define MESSAGE_RECEIVE "[DEBUG] Received message: "
+#define MESSAGE_SEND_SUCCESS "[DEBUG] Message sent"
