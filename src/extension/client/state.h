b08912dce4aba8d2bcee0f4155e7a7676b5daeca
Tom Stapely <tas225@ic.ac.uk>
2026-06-12 17:02:04 +0100

    [Hari] [Hugo Carter] [Tom Stapely] feat: designed packet structs to do exactly what needs to be done

--- src/extension/client/state.h
+++ src/extension/client/state.h
@@ -1,18 +1,35 @@
 #ifndef CLIENT_STATE_H
 #define CLIENT_STATE_H
 
-#include "logic.h"
-#include "shared/board.h"
 #include <stdbool.h>
+#include "shared/board.h"
+
+typedef enum {
+    UI_STATE_CONNECTING,
+    UI_STATE_PLACING_SHIPS,
+    UI_STATE_WAITING_FOR_OPPONENT,
+    UI_STATE_MY_TURN,
+    UI_STATE_OPPONENT_TURN,
+    UI_STATE_GAME_OVER
+} UIState;
 
 typedef struct {
-    ClientUIState ui_state;
-
-    Board own_board;
-    Board enemy_board;
-
-    bool is_my_turn;
+    // Network and status
+    int connection_fd;
+    UIState current_state;
+    bool is_running; // for loop condition
     bool i_won;
+
+    // Local data
+    Board my_board;
+    Board target_board;
+
+    // Temporary data when placing, irrelevant once main loop has begun
+    InitialShipDefs my_placements;
+    int ships_placed;
 } ClientState;
+
+extern ClientState init_client_state(void);
+extern void free_client_state(ClientState *state);
 
 #endif
--- src/extension/shared/board.h
+++ src/extension/shared/board.h
@@ -29,18 +29,24 @@ typedef struct {
     bool destroyed;
 } ShipState;
 
+typedef struct {
+    ShipType ship;
+    Position pos;
+} InitialShipState;
+
 typedef ShipState ShipDefs[NUM_SHIPS];
+
+typedef InitialShipState InitialShipDefs[NUM_SHIPS];
 
 /*
 Returns true if the ship placements form a valid board. If it is a valid placement, populate the board
 */
-extern bool board_is_valid_placement_set(ShipDefs ship_defs, Board board);
-extern bool board_is_valid_placement(ShipState sl, Board board, int nships);
+extern bool board_add_placement_set(InitialShipDefs ship_defs, Board board);
 
 extern Board create_empty_board(void);
 extern void free_board(Board board);
 
-extern bool board_try_hit(Board player_target_board, Board opponent_ships_board, Position pos);
+extern bool board_try_hit(Board opponent_ships_board, Position pos);
 extern bool all_ships_destroyed(Board board);
 
 extern void print_board(Board b, FILE *out);
