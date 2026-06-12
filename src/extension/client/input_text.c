eb0b1b819d9943fbb5f3b9e9412f881b1572aa4e
Hari <hn1125@ic.ac.uk>
2026-06-12 18:26:11 +0100

    [Hari] text input logic

---
+++ src/extension/client/input_text.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "input.h"
+
+#define BUFFER_SIZE 10
+
+InputData get_user_input(void) {
+    InputData data;
+
+    data.type = INPUT_NONE;
+    data.grid_x = 0;
+    data.grid_y = 0;
+
+    char input[BUFFER_SIZE];
+    if (fgets(input, BUFFER_SIZE, stdin) != NULL) {
+        if (strcmp(input, "quit")) {
+            data.type = INPUT_QUIT;
+        } else {
+            // expect grid coords
+            data.type = INPUT_SELECT_GRID;
+            char *start = input;
+            while (isspace(*start)) start++;
+            // expect digit
+            if (isdigit(*start) && atoi(start) < 11) {
+                data.grid_x = atoi(start);
+            } else {
+                data.type = INPUT_NONE;
+            }
+
+            while (isspace(*start)) start++;
+            // expect digit
+            if (isdigit(*start) && atoi(start) < 11) {
+                data.grid_y = atoi(start);
+            } else {
+                data.type = INPUT_NONE;
+            }
+        }
+    }
+
+    return data;
+}
