2dbcdbf6aa916d9eb0952588917dd138bdaefb33
Hugo Carter <hjc25@ic.ac.uk>
2026-06-19 15:08:34 +0100

    [Hugo Carter] fix(assemble): fix test compile

--- tests/assembler/pass1/test_scanner.c
+++ tests/assembler/pass1/test_scanner.c
@@ -4,10 +4,13 @@
 
 void test_scanfile(void) {
     SymbolTable *table = create_symbol_table();
-    scan_file("tests/assembler/pass1/test_scanner.txt", table);
+    FILE *out = fopen("tests/assembler/pass1/test_scanner.txt", "r");
+    assert (out != NULL);
+    scan_file(out, table);
     assert(symbol_table_lookup(table, ".label1") == 0);
     assert(symbol_table_lookup(table, "label2") == 12);
     assert(symbol_table_lookup(table, "_label$") == 20);
+    fclose(out);
 
     symbol_table_lookup(table, "_label$");
     free_symbol_table(table);
